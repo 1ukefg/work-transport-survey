c33bdd2ed91e776b003b025ab67b04e5bab046ab
1ukefg <lukefletchr@gmail.com>
2023-05-26 13:15:19 +0000

    Define a function that provides an introduction,a string asking for the first piece of information and an exmple. When run, the funtion will ask for the name to be entered and will return the entry.

--- run.py
+++ run.py
@@ -5,15 +5,23 @@ SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
-    ]
+]
 
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('work-transport-survey')
 
-methods = SHEET.worksheet('Methods')
 
-data = methods.get_all_values()
+def get_employee_name():
+    """
+    Retrieve the Emplyee name, mode of transport and distance travelled to work  from the user.
+    """
+    print("Thank you for taking the time to participate in the survey!")
+    print("Lets start of by taking your first and last name.")
+    print("An example entry could be: Tom Jerry\n")
 
-print(data)
+    input_string = input("Enter your name below:\n")
+    print(f"You entered {input_string}")
+
+get_employee_name()
